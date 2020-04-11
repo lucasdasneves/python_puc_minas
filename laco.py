7f8f2a479985da0adb7bc2becaa632463a57a2fd
lucasdasneves <lucas_neves5@hotmail.com>
2020-04-11 20:52:12 -0300

    Lists using for
    In this class I learned how to execute the command for for lists

---
+++ laco.py
@@ -0,0 +1,12 @@
+materiais = ['caneta', 'caderno', 'livro', 'lapis']
+print(materiais)
+
+#printing each element from the list
+
+materiais = ['caneta', 'caderno', 'livro', 'e-book']
+for material in materiais :
+    print(material) #this is the for body
+    print(material.title())
+    print(len(materiais))
+
+# for makes variable "material" write each element from the list per time
