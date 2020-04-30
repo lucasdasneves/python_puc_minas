bf367bd94d2354583b5657533604313835cb5920
lucasdasneves <62776635+lucasdasneves@users.noreply.github.com>
2020-04-30 08:48:19 -0300

    solved
    I commit a mitake in the file, I put the rtf extension

--- reading_file_python.py
+++ reading_file_python.py
@@ -1,8 +1,8 @@
 #reading files in python
 
-file = open('C:\\Users\\Samsung\\Documents\\GitHub\\python_puc_minas\\pessoas.txt','r')
+file = open("pessoas.txt","r")
 for linha in file:
-    lista = linha.split()
-    print(lista)
+	lista = linha.split()
+	print(lista)
 
 file.close()
