f365bb11d4d747a27a7242cffda214127e74a15a
lucasdasneves <62776635+lucasdasneves@users.noreply.github.com>
2020-04-30 20:53:47 -0300

    texting

--- reading_file_python.py
+++ reading_file_python.py
@@ -1,6 +1,9 @@
 #reading external files with python
 
-file = open("pessoas.txt","r")
+file = open("pessoas.txt","r") #r means that the archive is set for reading
+# for statements is used to run the rows from the file
+#split command splits the rows, the space gives the reference
+#close command is used to close the file
 for row in file:
 	list = row.split()
 	print(list)
