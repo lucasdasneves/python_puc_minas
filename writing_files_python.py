d98af0b0a0a917d98a2fd618ed08eb85b2e86c43
lucasdasneves <62776635+lucasdasneves@users.noreply.github.com>
2020-04-30 21:11:34 -0300

    writing

--- writing_files_python.py
+++ writing_files_python.py
@@ -6,5 +6,7 @@ file = open('materiais.txt','w')
 for material in materiais:
     file.write(material + '\n')
     # \n is used to wrap the row
+    #can I write my code in a Excel or SpreadSheets file?
+    #https://stackoverflow.com/questions/9690138/how-do-i-access-read-write-google-sheets-spreadsheets-with-python
 
 file.close()
