370c096b48e841d39bc1154583fd76538b67c489
lucasdasneves <62776635+lucasdasneves@users.noreply.github.com>
2020-05-01 20:30:51 -0300

    correction

--- Exercise1.py
+++ Exercise1.py
@@ -1,6 +1,18 @@
 #Exercise1
 
-i = input()
-for numb in i:
-    print(numb)
-print(sum(i))
+#i = input()
+#for numb in i:
+   # print(numb)
+#print(i)
+
+#quadrados = []
+#for valor in range(1,11):
+#quadrado = valor ** 2
+#quadrados.append(quadrado)
+#print (quadrados)
+
+soma = 0
+for i in range(1,11):
+    num = int(input())
+    soma = soma + num
+print(soma)
