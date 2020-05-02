a2c38d8d18860c6c7e5318571009f8d3a0005b4d
lucasdasneves <62776635+lucasdasneves@users.noreply.github.com>
2020-05-02 10:06:29 -0300

    new classes

---
+++ if_command_3.py
@@ -0,0 +1,17 @@
+#if command 3
+
+#and and or
+
+idade = 20
+if idade >= 18 and idade <= 69:
+    print ("Voto obrigatório")
+else:
+    print("Voto não obrigatório")
+
+nota = 80
+frequencia = 15
+if nota < 60 or frequencia > 25:
+    print("Reprovado")
+else:
+    print("Aprovado")
+    #eu tive de acrescentar um valor para frequencia para que funcionasse
---
+++ in_operator.py
@@ -0,0 +1,13 @@
+#in and not int operators
+
+materiais = ['caneta', 'caderno', 'livro', 'e-book']
+valor = 'caneta' in materiais
+print (valor)
+valor = 'agenda' in materiais
+print (valor)
+
+materiais = ['caneta', 'caderno', 'livro', 'e-book']
+valor = 'caneta' not in materiais
+print (valor)
+valor = 'agenda' not in materiais
+print (valor)
