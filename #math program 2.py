bd4414ac680844be182e6386a551ca621d9c3ce8
lucasdasneves <62776635+lucasdasneves@users.noreply.github.com>
2020-05-02 10:48:32 -0300

    exercises

---
+++ #math program 2.py
@@ -0,0 +1,10 @@
+#math program 2
+
+var1 = input("Insert the first variable: ")
+var2 = input("Insert the second variable: ")
+op = input()
+
+if op == 1: #warning !!: I need to put ==
+    print(var1 + var2)
+else:
+    print("Wrong operator selected")
---
+++ if_elif_else_syntax.py
@@ -0,0 +1,9 @@
+#if elif else instruction
+
+idade = 12
+if idade < 4:
+    print("Sua entrada custa R$0,00")
+elif idade < 18:
+    print("Sua entrada custa R$5,00")
+else:
+    print("Sua entrada custa R$10,00")
